0edf17e639367f0cb4b8483d16ea89823f8a39ed
Grand <grand.uk@tutanota.com>
2023-11-05 11:59:45 +0000

    final push

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ mod atproto;
 use atproto::ATP;
 use eframe::egui;
 
+#[derive(Default)]
+struct BlueskyClient {
+    identity: String,
+    provider: String,
+    password: String
+}
+
 fn main() {
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(320.0,240.0)),
@@ -11,35 +18,46 @@ fn main() {
     // let mut atproto = ATP::new(&base);
     println!("Provider: {}",base);
 
-    let _ = eframe::run_simple_native("Bluesky Unofficial Native",options, move |ctx, _frame| {
-        let mut provider = String::new();
-        let mut identity = String::new();
-        let mut password = String::new();
+    let mut provider = String::new();
+    let mut identity = String::new();
+    let mut password = String::new();
+
+
+    eframe::run_native("Bluesky Native", options, Box::new(|cc| Box::new(BlueskyClient::new(cc))));
+}
+
+impl BlueskyClient {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+impl eframe::App for BlueskyClient {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Login");
             ui.horizontal(|ui| {
                 let provider_label = ui.label("Provider: ");
-                ui.text_edit_singleline(&mut provider)
+                ui.text_edit_singleline(&mut self.provider)
                     .labelled_by(provider_label.id);
             });
 
             ui.horizontal(|ui| {
                 let identity_label = ui.label("Identitifier: ");
-                ui.text_edit_singleline(&mut identity)
+                ui.text_edit_singleline(&mut self.identity)
                     .labelled_by(identity_label.id);
             });
             
             ui.horizontal(|ui| {
                 let password_label = ui.label("Password: ");
-                ui.text_edit_singleline(&mut password)
+                ui.text_edit_singleline(&mut self.password)
                     .labelled_by(password_label.id);
             });
             if ui.button("Login").clicked() {
-                let mut atproto = ATP::new(&provider);
-                match atproto.login(&identity, password) {
+                let mut atproto = ATP::new(&self.provider);
+                match atproto.login(&self.identity, self.password.to_string()) {
                     Ok(_) => {
                         println!("logged in");
-                        ui.label(format!("Logged in as {}", identity));
+                        ui.label(format!("Logged in as {}", self.identity));
 
                         let mut post_text = String::new();
                         let post_text_label = ui.label("Post:");
@@ -57,5 +75,5 @@ fn main() {
                 }
             }
         });
-    });
+    }
 }
