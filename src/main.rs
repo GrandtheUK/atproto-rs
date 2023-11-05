69d79295b938a23b04f000d257e194652cd6c8cb
Grand <grand.uk@tutanota.com>
2023-11-05 11:22:08 +0000

    added posting gui logic

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,18 @@ fn main() {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Login");
             ui.horizontal(|ui| {
-                
                 let provider_label = ui.label("Provider: ");
                 ui.text_edit_singleline(&mut provider)
                     .labelled_by(provider_label.id);
             });
+
             ui.horizontal(|ui| {
-                
                 let identity_label = ui.label("Identitifier: ");
                 ui.text_edit_singleline(&mut identity)
                     .labelled_by(identity_label.id);
             });
+            
             ui.horizontal(|ui| {
-                
                 let password_label = ui.label("Password: ");
                 ui.text_edit_singleline(&mut password)
                     .labelled_by(password_label.id);
@@ -41,6 +40,18 @@ fn main() {
                     Ok(_) => {
                         println!("logged in");
                         ui.label(format!("Logged in as {}", identity));
+
+                        let mut post_text = String::new();
+                        let post_text_label = ui.label("Post:");
+                        ui.text_edit_multiline(&mut post_text)
+                            .labelled_by(post_text_label.id);
+                        if ui.button("Post").clicked() {
+                            match atproto.post(post_text) {
+                                Ok(_) => println!("Post made"),
+                                Err(_) => println!("Post could not be made")
+                            }
+                        }
+
                     },
                     Err(_) => println!("Not Logged in")
                 }
