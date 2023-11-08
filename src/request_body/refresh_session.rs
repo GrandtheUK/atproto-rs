472620e0e15928b0500f65fc4aa97e1826307ef0
Grand <grand.uk@tutanota.com>
2023-11-08 10:30:26 +0000

    removed getters crate from refreshsession

--- src/request_body/refresh_session.rs
+++ src/request_body/refresh_session.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize,Serialize};
-use derive_getters::Getters;
 
-#[derive(Serialize,Deserialize,Getters,Debug)]
+#[derive(Serialize,Deserialize,Debug)]
 #[allow(non_snake_case)]
 pub struct RefreshSessionRes {
     pub accessJwt: String,
